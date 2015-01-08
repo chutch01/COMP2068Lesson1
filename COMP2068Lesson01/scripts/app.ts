04faf9946d7ae98bc69030398c2c431399d67ec1
chutch01 <chutch01@hotmail.com>
2015-01-08 13:33:56 -0500

    added basic structure for typescript and jumping

---
+++ COMP2068Lesson01/scripts/app.ts
@@ -0,0 +1,15 @@
+﻿class Player {
+
+    strength: number;
+    contructor() {
+        this.strength = 10;
+    }
+    jump() {
+        console.debug("you jumped!");
+    }
+}
+
+function main() {
+    var tom = new Player();
+    tom.jump();
+}
